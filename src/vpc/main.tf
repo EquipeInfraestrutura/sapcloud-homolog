5aaa3d4b4c7ebbcebf0c2595fd1b9536d0c656ed
Infraestrutura <equipe.infraestrutura@ias.org.br>
2024-01-11 11:15:52 -0300

    deploy da nova arquitetura do SAP1

--- src/vpc/main.tf
+++ src/vpc/main.tf
@@ -51,16 +51,21 @@ resource "aws_route" "private" {
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.default.id
 
+  route{
+    route_table_id         = aws_route_table.public.id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id             = "${aws_internet_gateway.default.id},${aws_vpn_gateway.vpg_projetosap.id}"   
+  } 
     tags = {
      Name = "rt_public"
   }
 }
 
-resource "aws_route" "public" {
-  route_table_id         = aws_route_table.public.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.default.id},${aws_vpn_gateway.vpg_projetosap.id}"
-}
+# resource "aws_route" "public" {
+#   route_table_id         = aws_route_table.public.id
+#   destination_cidr_block = "0.0.0.0/0"
+#   gateway_id             = "${aws_internet_gateway.default.id},${aws_vpn_gateway.vpg_projetosap.id}"
+# }
 
 resource "aws_subnet" "private" {
   count = length(var.private_subnet_cidr_blocks)
