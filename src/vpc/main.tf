3a2d4edde7ee82ecb1cf02333f105040963f29ed
Infraestrutura <equipe.infraestrutura@ias.org.br>
2024-01-11 09:13:27 -0300

    inserção de array de gateways na route publica

--- src/vpc/main.tf
+++ src/vpc/main.tf
@@ -47,8 +47,8 @@ resource "aws_route_table" "public" {
 resource "aws_route" "public" {
   route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = [aws_internet_gateway.default.id,
-                            aws_vpn_gateway.vpg_projetosap.id                             
+  gateway_id             = ["aws_internet_gateway.default.id",
+                            "aws_vpn_gateway.vpg_projetosap.id"                             
   ]
 }
 
